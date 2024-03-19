6935b4b31d3f7205f67f979ee11b1106f4dcb531
dandanarq <merchanmd@gmail.com>
2024-03-18 22:03:42 -0500

    chore: initial commit, create backend, install node, express and nodemon

---
+++ index.js
@@ -0,0 +1,10 @@
+//! have settings of the server
+
+const express = require('express')
+
+const app = express()
+
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en el purto 4000')
+})
