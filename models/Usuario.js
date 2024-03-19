4866fbaf2c292574bbf0a2621faa0044014c7b3d
dandanarq <merchanmd@gmail.com>
2024-03-19 16:31:27 -0500

    chore: create db model Usuario using mongoose

---
+++ models/Usuario.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// defeni schema db
+const usuarioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        dafault: false,
+    }
+}, {
+    // crea dos propiedades mas: created and updated
+    timestamps: true,
+})
+
+const Usuario = mongoose.model('Usuario', usuarioSchema)
+export default Usuario
