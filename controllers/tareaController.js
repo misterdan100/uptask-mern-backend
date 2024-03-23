dc8fa5c1a90cfff670e23eeb23f588ce08d9654f
dandanarq <merchanmd@gmail.com>
2024-03-22 21:42:37 -0500

    feat: add new task relating to a project

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,5 +1,28 @@
+import Proyecto from "../models/Proyecto.js"
+import Tarea from "../models/Tarea.js"
+
 const agregarTarea = async (req, res) => {
-    res.json({msg: 'funcion agregarTarea'})
+    // check if project exist
+    const { proyecto } = req.body
+    try {
+        const existeProyecto = await Proyecto.findById(proyecto)
+        if(!existeProyecto) {
+            const error = new Error('El proyecto no existe')
+            return res.status(404).json({msg: error.message})
+        }
+
+        if(existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+            const error = new Error('No tienes los permisos para agregar tareas a este proyecto.')
+            return res.status(404).json({msg: error.message})
+        }
+
+        const tareaAlmacenada = await Tarea.create(req.body)
+        res.json('Tarea creada con exito')
+        
+    } catch (error) {
+        res.json({msg: "Hubo un error!"})
+        return console.log(error.message)
+    }
 }
 
 const obtenerTarea = async (req, res) => {
