ad40f0f42dfe84a37cd85436592ab0ae4e4b3272
dandanarq <merchanmd@gmail.com>
2024-03-22 22:19:09 -0500

    feat: get task by id, validating creator

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,4 +1,5 @@
 import Proyecto from "../models/Proyecto.js"
+import Usuario from "../models/Usuario.js"
 import Tarea from "../models/Tarea.js"
 
 const agregarTarea = async (req, res) => {
@@ -13,11 +14,11 @@ const agregarTarea = async (req, res) => {
 
         if(existeProyecto.creador.toString() !== req.usuario._id.toString()) {
             const error = new Error('No tienes los permisos para agregar tareas a este proyecto.')
-            return res.status(404).json({msg: error.message})
+            return res.status(401).json({msg: error.message})
         }
 
         const tareaAlmacenada = await Tarea.create(req.body)
-        res.json('Tarea creada con exito')
+        res.status(201).json('Tarea creada con exito')
         
     } catch (error) {
         res.json({msg: "Hubo un error!"})
@@ -26,7 +27,26 @@ const agregarTarea = async (req, res) => {
 }
 
 const obtenerTarea = async (req, res) => {
-    res.json({msg: 'funcion obtenerTarea'})
+    const { id } = req.params
+    try {
+        const tarea = await Tarea.findById(id).populate("proyecto")
+
+        if(!tarea) {
+            const error = new Error('Tarea no encontrada.')
+            return res.status(404).json({msg: error.message})
+        }
+
+        if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+            const error = new Error('No tienes permisos para esta tarea.')
+            return res.status(401).json({msg: error.message})
+        }
+
+        return res.status(200).json(tarea)
+    } catch (error) {
+        const error2 = new Error('Hubo un error!')
+        res.status(404).json({msg: error2.message})
+        return console.log(error.message) 
+    }
 }
 
 const actualizarTarea = async (req, res) => {
