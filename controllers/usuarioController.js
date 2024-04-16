8e3f0c9cac31bab2f53251b2a39f6837fc8a678b
dandanarq <merchanmd@gmail.com>
2024-04-15 20:48:25 -0500

    fix: response in authentication function w/ user data

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -157,8 +157,7 @@ const nuevoPassword = async (req, res) => {
 const perfil = async (req, res) => {
     const { usuario } = req
     
-    // res.json({usuario})
-    console.log(usuario)
+    res.json({usuario})
 }
 
 
